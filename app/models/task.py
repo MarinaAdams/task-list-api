afca48e1c4da667059a0994575140921b69b48c3
Marina Adams <bardakover@gmail.com>
2025-05-09 00:43:07 -0700

    Added task routes, route helper functions; validated models, tested with Postman; passed Wave 1 tests

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,26 @@
 from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
 from ..db import db
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+    description: Mapped[str]
+    completed_at: Mapped[datetime] = mapped_column(nullable=True)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "is_complete": self.completed_at is not None
+            }
+        
+    @classmethod
+    def from_dict(cls, task_data):
+        return cls(
+            title=task_data["title"],
+            description=task_data["description"],
+            completed_at=task_data.get("completed_at")
+            )
+            
--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,1 +1,40 @@
-from flask import Blueprint
+from flask import Blueprint, request, Response
+from app.models.task import Task
+from .route_utilities import validate_model, create_model, get_models_with_filters
+from ..db import db
+from flask import jsonify
+
+bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
+
+@bp.post("")
+def create_task():
+    request_body = request.get_json()
+    return create_model(Task, request_body)
+
+@bp.get("")
+def get_all_tasks():
+    return get_models_with_filters(Task, request.args)
+
+@bp.get("/<task_id>")
+def get_one_task(task_id):
+    task = validate_model(Task, task_id)
+    return jsonify({"task": task.to_dict()})
+
+@bp.put("/<task_id>")
+def update_task(task_id):
+    task = validate_model(Task, task_id)
+    request_body = request.get_json()
+
+    task.title = request_body["title"]
+    task.description = request_body["description"]
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json") # 204 No Content
+
+@bp.delete("/<task_id>")
+def delete_task(task_id):
+    task = validate_model(Task, task_id)
+    db.session.delete(task)
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
