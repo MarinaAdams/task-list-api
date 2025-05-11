eb67c68a4f665a91f3ca98b400cbc157e4f38034
Marina Adams <bardakover@gmail.com>
2025-05-10 20:59:09 -0700

    Finished Wave 4: Edited task routes. Implemented Slack Bot. Refactored PATCH/mark_complete.

--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -2,7 +2,11 @@ from flask import Blueprint, request, Response, make_response
 from app.models.task import Task
 from .route_utilities import validate_model, create_model, get_models_with_filters
 from ..db import db
+from dotenv import load_dotenv
+load_dotenv()
 from datetime import datetime
+import os
+import requests
 
 bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
 
@@ -25,6 +29,36 @@ def get_all_tasks():
     tasks = db.session.scalars(query).all()  
 
     return [task.to_dict() for task in tasks], 200  
+
+@bp.patch("/<task_id>/mark_complete")
+def mark_complete(task_id):
+    task = validate_model(Task, task_id)
+    task.completed_at = datetime.today()
+    db.session.commit()
+
+    slack_url = "https://slack.com/api/chat.postMessage"
+    slack_token = os.environ.get("SLACKBOT_TOKEN")
+
+    headers = {
+        "Authorization": f"Bearer {slack_token}",
+        "Content-Type": "application/json"
+    }
+
+    data = {
+        "channel": "test-slack-api",
+        "text": f"Someone just completed the task {task.title}"
+    }
+
+    response = requests.post(slack_url, headers=headers, json=data)  
+    return make_response(task.to_dict(), 200)
+
+@bp.patch("/<task_id>/mark_incomplete")
+def mark_incomplete(task_id):
+    task = validate_model(Task, task_id)
+    task.completed_at = None
+    db.session.commit()
+    return Response(status=204, mimetype="application/json")
+
 
 @bp.get("/<task_id>")
 def get_one_task(task_id):
@@ -50,16 +84,3 @@ def delete_task(task_id):
 
     return Response(status=204, mimetype="application/json")
 
-@bp.patch("/<task_id>/mark_complete")
-def mark_complete(task_id):
-    task = validate_model(Task, task_id)
-    task.completed_at = datetime.utcnow()
-    db.session.commit()
-    return Response(status=204, mimetype="application/json")
-
-@bp.patch("/<task_id>/mark_incomplete")
-def mark_incomplete(task_id):
-    task = validate_model(Task, task_id)
-    task.completed_at = None
-    db.session.commit()
-    return Response(status=204, mimetype="application/json")
